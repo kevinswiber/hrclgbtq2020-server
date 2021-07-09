5f84dc843ad2f8e160eef31aa9766d28d6f4ac61
Kevin Swiber <kswiber@gmail.com>
2021-07-09 15:34:45 -0700

    moving class component to function component.

--- app/src/components/IssuesByStateBarChart.js
+++ app/src/components/IssuesByStateBarChart.js
@@ -1,92 +1,86 @@
-import { HashHistory } from '@reach/router';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as d3 from 'd3';
 
-export class IssuesByStateBarChart extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { current: props.current || 'choose' };
-  }
+export const IssuesByStateBarChart = (props) => {
+  const [current, setCurrent] = useState(props.current || 'choose');
 
-  componentDidUpdate() {
-    window.location.hash = `#${this.state.current}`;
-  }
+  useEffect(() => {
+    window.location.hash = `#${current}`;
+  });
 
-  render() {
-    const current = this.state.current;
-    const sorted = this.props.states.sort((a, b) => d3.ascending(a.name, b.name));
-    const change = (event) => {
-      this.setState({ current: event.target.value })
-    };
+  const sorted = props.states.sort((a, b) => d3.ascending(a.name, b.name));
+  const change = (event) => {
+    setCurrent(event.target.value);
+  };
 
-    const select = (
-      <select onChange={change} value={current}>
-        <option value="choose">-- Choose a state --</option>
-        {sorted.map((d) => {
-          return (
-            <option key={d.id} value={d.id} >{d.name}</option>
-          )
-        })}
-      </select>
-    );
+  const select = (
+    <select onChange={change} value={current}>
+      <option value="choose">-- Choose a state --</option>
+      {sorted.map((d) => {
+        return (
+          <option key={d.id} value={d.id} >{d.name}</option>
+        )
+      })}
+    </select>
+  );
 
-    if (this.state.current.toLowerCase() === 'choose') {
-      return (
-        <div id="issues-by-state-chart">
-          {select}
-        </div>
-      );
-    }
-
-    const data = this.props.states.find((s) => s.id === this.state.current).issues;
-
-    const margin = { top: 30, right: 60, bottom: 10, left: 60 };
-    const barHeight = 25;
-    const height = Math.ceil((data.length + 0.1) * barHeight) + margin.top + margin.bottom;
-    const width = 1024;
-
-    let xDomain = d3.extent(data, (d) => d.value);
-    if (xDomain[0] > -2) {
-      xDomain[0] = -2;
-    }
-    if (xDomain[1] < 2) {
-      xDomain[1] = 2;
-    }
-
-    const x = d3.scaleLinear()
-      .domain(xDomain)
-      .rangeRound([margin.left, width - margin.right]);
-
-    const y = d3.scaleBand()
-      .domain(d3.range(data.length))
-      .rangeRound([margin.top, height - margin.bottom])
-      .padding(0.1);
-
+  if (current.toLowerCase() === 'choose') {
     return (
       <div id="issues-by-state-chart">
         {select}
-        <svg viewBox={`0,0,${width},${height}`}>
-          <g>
-            {data.map((d, i) => {
-              let width = Math.abs(x(d.value) - x(0));
-              if (width === 0) {
-                width = 2;
-              }
+      </div>
+    );
+  }
 
-              return (
-                <g key={d.kind}>
-                  <rect
-                    fill={d3.schemeSet1[d.value >= 0 ? 1 : 0]}
-                    x={x(Math.min(d.value, 0))}
-                    y={y(i)}
-                    width={width}
-                    height={y.bandwidth()} />
-                  <title>{d.policy}</title>
-                </g>
-              );
-            })}
-          </g>
-          {/*
+  const data = props.states.find((s) => s.id === current).issues;
+
+  const margin = { top: 30, right: 60, bottom: 10, left: 60 };
+  const barHeight = 25;
+  const height = Math.ceil((data.length + 0.1) * barHeight) + margin.top + margin.bottom;
+  const width = 1024;
+
+  let xDomain = d3.extent(data, (d) => d.value);
+  if (xDomain[0] > -2) {
+    xDomain[0] = -2;
+  }
+  if (xDomain[1] < 2) {
+    xDomain[1] = 2;
+  }
+
+  const x = d3.scaleLinear()
+    .domain(xDomain)
+    .rangeRound([margin.left, width - margin.right]);
+
+  const y = d3.scaleBand()
+    .domain(d3.range(data.length))
+    .rangeRound([margin.top, height - margin.bottom])
+    .padding(0.1);
+
+  return (
+    <div id="issues-by-state-chart">
+      {select}
+      <svg viewBox={`0,0,${width},${height}`}>
+        <g>
+          {data.map((d, i) => {
+            let width = Math.abs(x(d.value) - x(0));
+            if (width === 0) {
+              width = 2;
+            }
+
+            return (
+              <g key={d.kind}>
+                <rect
+                  fill={d3.schemeSet1[d.value >= 0 ? 1 : 0]}
+                  x={x(Math.min(d.value, 0))}
+                  y={y(i)}
+                  width={width}
+                  height={y.bandwidth()} />
+                <title>{d.policy}</title>
+              </g>
+            );
+          })}
+        </g>
+        {/*
         <g fontFamily="sans-serif" fontSize="10">
           {data.map((d, i) => {
             return (
@@ -101,25 +95,24 @@ export class IssuesByStateBarChart extends React.Component {
           })}
         </g>
         */}
-          <g transform={`translate(${x(0)},0)`}
-            ref={(node) => d3.select(node)
-              .call(d3.axisLeft(y)
-                .tickFormat((i) => data[i].name)
-                .tickSize(0)
-                .tickPadding(5))
-              .call((g) => g.selectAll('.tick text')
-                .filter(i => data[i].value < 0)
-                .attr('text-anchor', 'start')
-                .attr('x', 6))}>
-          </g>
-          <g transform={`translate(0,${margin.top})`}
-            ref={(node) => d3.select(node)
-              .call(d3.axisTop(x)
-                .ticks(x.domain()[1] - x.domain()[0])
-              )}>
-          </g>
-        </svg>
-      </div>
-    );
-  }
+        <g transform={`translate(${x(0)},0)`}
+          ref={(node) => d3.select(node)
+            .call(d3.axisLeft(y)
+              .tickFormat((i) => data[i].name)
+              .tickSize(0)
+              .tickPadding(5))
+            .call((g) => g.selectAll('.tick text')
+              .filter(i => data[i].value < 0)
+              .attr('text-anchor', 'start')
+              .attr('x', 6))}>
+        </g>
+        <g transform={`translate(0,${margin.top})`}
+          ref={(node) => d3.select(node)
+            .call(d3.axisTop(x)
+              .ticks(x.domain()[1] - x.domain()[0])
+            )}>
+        </g>
+      </svg>
+    </div>
+  );
 };
